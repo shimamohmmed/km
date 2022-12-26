447d27a72d06ba43aa8790290c245846d1856f76
shimamohmmed <121094945+shimamohmmed@users.noreply.github.com>
2022-12-26 15:06:25 +0200

    Add files via upload

---
+++ home.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Alaawebite</title>
+    <meta charset="UTF-8">
+    <meta name="description" content=" موقع شحصي" >
+    <link rel="stylesheet" href="moon.css">
+</head>
+<body>
+    <center>
+    <header>
+        <div clas="container">
+        <div id ="brand">
+            <h1>موقع ميكب</h1>
+    </div>
+    <nav>
+
+        <ul>
+            <li><a href="home.html">الصفحه الرئسيه</a></li>
+            </li><a href=" # ">معرض الاعمال</a></li>
+            <li><a href=" # ">اتصل بنا</a></li>
+            <li><a href=" # " > قالو عنا</a</li>
+        </ul>
+        <div>
+        <background:url('img/mm3.jpg')>
+</div>
+    </nav>
+    </div>
+    </header>
+    <section id="newsletter">
+        <div class="container">
+            <h1>اشترك بالنشره البريديه</h1>
+            <form action="">
+                <input type="email" placeholder="ادخل بريدك الالكتروني">
+                <butten type="submit"class="button">اشترك</butten>
+            </form>
+        </div>
+    </section>
+            <section id="boxes">
+                <div class="container">
+                    <div class="box">
+                        <p>
+                            <img src="logo.jpg" alt="logo"width="450px">
+                        </p>
+                        <h2>تصاميم احترافيه</h2>
+                    </div> 
+                      <div class="box">
+                        <h2> تسليم بالوقت المحدد</h2>
+                    </div>
+
+  <div class="box">
+                        <h2>استخدام احدث الماركات</h2>
+                    </div> 
+                </div>
+            </section>
+    
+</center>
+</body>
+<footer><p>موقع تسويق اون لاين  &copy;2022</p>
+
+</footer>
+</html>
+
+
+
+ذ
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
